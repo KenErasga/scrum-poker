d40d15a8221350b05457c485709a4cea83000572
“Ken <kenneth.erasga@gmail.com>
2020-08-20 13:23:06 +0100

    store in session storage instead of local storage

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,8 @@ Amplify.configure({
       mandatorySignIn: true,
       region: process.env.REACT_APP_COGNITO_REGION,
       userPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID,
-      userPoolWebClientId: process.env.REACT_APP_COGNITO_APP_CLIENT_ID
+      userPoolWebClientId: process.env.REACT_APP_COGNITO_APP_CLIENT_ID,
+      storage: window.sessionStorage
     },
 });
 
