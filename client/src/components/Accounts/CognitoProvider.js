23b7d746da2752540aa0c289014d4608cc76e3ff
“Ken <kenneth.erasga@gmail.com>
2020-08-24 12:54:06 +0100

    spaces

--- client/src/components/Accounts/CognitoProvider.js
+++ client/src/components/Accounts/CognitoProvider.js
@@ -1,46 +1,51 @@
-import {Auth} from 'aws-amplify'
+import { Auth } from 'aws-amplify'
 import React, { createContext } from 'react'
 
 const AccountContext = createContext();
-const Account = props => {
 
-    const signUp = async (room, password) => {
-        try {
-            return await Auth.signUp({
-                username: room,
-                password: password
-            })
-        } catch (e) {
-            console.error(e.message)
-            throw new Error(e)
-        }
-    };
+const AuthContext = createContext();
 
-    const signIn = async (room, password) => {
-        try {
-            await Auth.signIn(room, password);
-          } catch (e) {
-            console.error(e.message)
-            throw new Error(e)
-          }
-    };
+const signUp = async (room, password) => {
+    try {
+        return await Auth.signUp({
+            username: room,
+            password: password
+        })
+    } catch (e) {
+        console.error(e.message)
+        throw new Error(e)
+    }
+};
 
-    const logout = async () => {
+const signIn = async (room, password) => {
+    try {
+        await Auth.signIn(room, password);
+    } catch (e) {
+        console.error(e.message)
+        throw new Error(e)
+    }
+};
+
+const logout = async () => {
+    try {
         await Auth.signOut();
         console.log("logout");
-    };  
+    } catch (e) {
+        console.error(e.message)
+        throw new Error(e)
+    }
+};
 
+const Account = props => {
     return (
         <AccountContext.Provider value={{
             signIn,
             signUp,
             logout,
         }}>
-                {props.children}
+            {props.children}
         </AccountContext.Provider>
     );
 };
-
-const AuthContext = createContext();
 
 export { Account, AccountContext, AuthContext }
--- client/src/components/Dropdown/Dropdown.js
+++ client/src/components/Dropdown/Dropdown.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {FormControl, InputLabel, NativeSelect} from '@material-ui/core'
+import { FormControl, InputLabel, NativeSelect } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -12,24 +12,24 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const DropDownList = ({number, numberList, setNumber}) => {
-    const classes = useStyles();
-    return (
-        <div>
-        <FormControl className={classes.formControl}>
+const DropDownList = ({ number, numberList, setNumber }) => {
+  const classes = useStyles();
+  return (
+    <div>
+      <FormControl className={classes.formControl}>
         <InputLabel id="estimateLabel">Estimate</InputLabel>
         <NativeSelect
           value={number}
           onChange={e => setNumber(e.target.value)}
         >
           <option key={`optionnone`} value=''></option>
-            {numberList.map(number => {
-                return (<option key={`option${number}`} value={number}>{number}</option>)
-            })}
+          {numberList.map(number => {
+            return (<option key={`option${number}`} value={number}>{number}</option>)
+          })}
         </NativeSelect>
       </FormControl>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default DropDownList;
