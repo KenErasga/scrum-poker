a6fb18e34c894127625c271b3d511364f18bd007
ale8k <alexanderkilroy@outlook.com>
2020-09-02 08:56:51 +0100

    Address PR comment
    Had excess white space, removed.

--- server/src/socketio-events/scrum-poker/constants/EVENT_CONSTANTS.ts
+++ server/src/socketio-events/scrum-poker/constants/EVENT_CONSTANTS.ts
@@ -2,5 +2,3 @@ export const USER_JOIN = "join";
 export const USER_SEND_ESTIMATE = "sendEstimate";
 export const USER_VIEW_ESTIMATES = "clickExpand";
 export const USER_DISCONNECT = "disconnect";
-
-
