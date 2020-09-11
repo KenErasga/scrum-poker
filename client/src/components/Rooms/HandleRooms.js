e494180b947b08bb6d28ce1110f9e9f8ca1d0984
ale8k <alexanderkilroy@outlook.com>
2020-09-11 09:45:17 +0100

    CognitoIdentityServiceProvider
    Just grabbed the users in prep for ticket.

--- client/src/components/Rooms/HandleRooms.js
+++ client/src/components/Rooms/HandleRooms.js
@@ -2,9 +2,30 @@ import React from 'react'
 import CreateRoom from './CreateRoom';
 import JoinRoom from './JoinRoom';
 import { Paper, Grid, Container } from '@material-ui/core';
+import AWS, { CognitoIdentityServiceProvider, Credentials } from "aws-sdk";
 
-const HandleRooms = () => (
-  <Container component="main" maxWidth="sm">
+const HandleRooms = () => {
+  AWS.config.update({
+    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.REACT_APP_AWS_SECRET_KEY_ID,
+    region: process.env.REACT_APP_COGNITO_REGION
+  });
+
+  AWS.config.getCredentials(function(err) {
+    if (err) console.log(err.stack);
+    // credentials not loaded
+    else {
+      console.log("Access key:", AWS.config.credentials.accessKeyId);
+    }
+  });
+  
+  new CognitoIdentityServiceProvider().listUsers({
+    UserPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID
+  }, (err, data) => {
+    console.log("boop.", err, data)
+  });
+
+  return (<Container component="main" maxWidth="sm">
     <Grid container spacing={3}>
       <Grid item xs={8}>
       </Grid>
@@ -19,7 +40,7 @@ const HandleRooms = () => (
         </Paper>
       </Grid>
     </Grid>
-  </Container>
-);
+  </Container>)
+};
 
 export default HandleRooms;
