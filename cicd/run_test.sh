0a030e08de4efb891a5d90aa693e4bc6424e0858
“Ken <kenneth.erasga@gmail.com>
2020-11-17 14:27:26 +0000

    update test script for pipeline

--- cicd/run_test.sh
+++ cicd/run_test.sh
@@ -3,7 +3,13 @@
 set -e
 
 ls
-
+echo "Node Version: $(node --version)"
+echo "NPM Version: $(npm --version)"
+ls
+cd scrum-poker/client
+ls
+yarn
+yarn test
 # task script is in resource-tutorial/job-inputs/ folder
 # application input is in gopath/src/github.com/cloudfoundry-community/simple-go-web-app folder
 # $GOPATH is gopath/ folder
