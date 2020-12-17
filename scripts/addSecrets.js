5065e4aad54fe93f947ca6b06429130ee8676f0f
“Ken <kenneth.erasga@gmail.com>
2020-12-17 15:30:02 +0000

    update script

--- scripts/addSecrets.js
+++ scripts/addSecrets.js
@@ -2,9 +2,9 @@ const { exec } = require("child_process");
 
 module.exports = function addSecrets() {
     exec("kubectl create secret generic aws-credentials -n my-release-main\
-    --from-file=aws-access-key=../aws-access-key.txt \
-    --from-file=aws-secret-key=../aws-secret-key.txt \
-    --from-file=aws-repo=../aws-repo.txt", (error, stdout, stderr) => {
+    --from-file=aws-access-key=./aws-access-key.txt \
+    --from-file=aws-secret-key=./aws-secret-key.txt \
+    --from-file=aws-repo=./aws-repo.txt", (error, stdout, stderr) => {
     if (error) {
         console.log(`error on aws-credentials secret creation: ${error}`);
         return;
@@ -17,10 +17,10 @@ module.exports = function addSecrets() {
     });
 
     exec("kubectl create secret generic react-app-credentials -n my-release-main\
-    --from-file=cognito-region=../cognito-region.txt \
-    --from-file=cognito-user-pool-id=../cognito-user-pool-id.txt \
-    --from-file=cognito-app-client-id=../cognito-app-client-id.txt  \
-    --from-file=socket-io-host=../socket-io-host.txt", (error, stdout, stderr) => {
+    --from-file=cognito-region=./cognito-region.txt \
+    --from-file=cognito-user-pool-id=./cognito-user-pool-id.txt \
+    --from-file=cognito-app-client-id=./cognito-app-client-id.txt  \
+    --from-file=socket-io-host=./socket-io-host.txt", (error, stdout, stderr) => {
     if (error) {
         console.log(`error on react-app-credentials secret creation: ${error}`);
         return;
